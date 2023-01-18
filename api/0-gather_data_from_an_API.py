ad508d455f611721822e6bfdb298cb0617314bb9
Dfunn1k <danychavezdev@gmail.com>
2023-01-18 13:21:21 -0500

    feat: added new function that request api

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    first_request = requests.get(url + argv[1])
+    employee_name = first_request.json()
+    second_requests = requests.get(url + argv[1] + '/todos/')
+    count = 0
+    task = second_requests.json()
+    title = ""
+
+    for item in task:
+        if item['completed'] is True:
+            if item != task[19]:
+                title += "\t {}\n".format(item['title'])
+            else:
+                title += "\t {}".format(item['title'])
+            count += 1
+
+    print("""Employee {} is done with tasks({}/20):
+    {}""".format(employee_name['name'], count, title), end='')
