5261aa0034f056df24f3fc1a99ea076032087caf
Dfunn1k <danychavezdev@gmail.com>
2023-01-18 17:28:51 -0500

    feat: added new documentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ url_base = 'https://jsonplaceholder.typicode.com/users/'
 
 
 def get_data():
-    """ This function get data of the placeholders API"""
+    """ This function get data of the placeholders API """
     name = requests.get(url_base + argv[1]).json()
     todos = requests.get(url_base + argv[1] + '/todos/').json()
     count = 0
@@ -21,8 +21,9 @@ def get_data():
             title += "\t {}\n".format(item['title'])
             count += 1
 
-    output = f"Employee {name['name']} is done with tasks({count}/20):"
-    print(output + f"\n{title}", end='')
+    print("Employee {} is done with tasks({}/20):\n{}".format(name['name'],
+                                                              count, title),
+          end='')
 
 
 if __name__ == '__main__':
