83fce666584235fab0dfc0db95481a55918ecd14
Dfunn1k <danychavezdev@gmail.com>
2023-01-18 18:03:14 -0500

    feat: added script that export data in the csv format

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module task1 about how to export data in the CSV format
+"""
+
+from sys import argv
+from requests import get
+from csv import writer
+
+url_base = 'https://jsonplaceholder.typicode.com/users/'
+file_name = 'USER_ID.csv'
+
+
+def export_csv():
+    """"""
+    usr = get(url_base + argv[1]).json()
+    tasks = get(url_base + argv[1] + '/todos').json()
+
+    for task in tasks:
+        data = [usr['id'], usr['username'], task['completed'], task['title']]
+        with open(file_name, 'a', encoding='utf-8') as csvfile:
+            csvwrite = writer(csvfile)
+            csvwrite.writerow(data)
+
+
+if __name__ == '__main__':
+    export_csv()
