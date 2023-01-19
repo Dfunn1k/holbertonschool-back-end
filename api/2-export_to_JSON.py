8ef3e82bbf91566efab9239f809e1ab056683194
Dfunn1k <danychavezdev@gmail.com>
2023-01-18 22:59:17 -0500

    feat: fix format of the JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -14,13 +14,17 @@ def export_json():
     """function that retrieve data in format json"""
 
     file_name = argv[1] + '.json'
+    usr = get(url_base + argv[1]).json()
     todos = get(url_base + argv[1] + '/todos/').json()
     items_data = []
     retrieve_json = dict()
 
     for todo in todos:
-        del todo['userId']
-        items_data.append(todo)
+        item_dict = dict()
+        item_dict['task'] = todo['title']
+        item_dict['completed'] = todo['completed']
+        item_dict['username'] = usr['username']
+        items_data.append(item_dict)
 
     retrieve_json[argv[1]] = items_data
 
