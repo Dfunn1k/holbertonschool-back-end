f914cc393ad8b396e690d868d825a55e65a43537
Dfunn1k <danychavezdev@gmail.com>
2023-01-19 14:34:37 -0500

    feat: added script that export data in the JSON format

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -10,12 +10,12 @@ from requests import get
 url_base = 'https://jsonplaceholder.typicode.com/users/'
 
 
-def export_json():
-    """function that retrieve data in format json"""
+def export_json(id):
+    """ function that retrieve data in format json """
 
-    file_name = argv[1] + '.json'
-    usr = get(url_base + argv[1]).json()
-    todos = get(url_base + argv[1] + '/todos/').json()
+    file_name = str(id) + '.json'
+    usr = get(url_base + str(id)).json()
+    todos = get(url_base + str(id) + '/todos/').json()
     items_data = []
     retrieve_json = dict()
 
@@ -26,11 +26,11 @@ def export_json():
         item_dict['username'] = usr['username']
         items_data.append(item_dict)
 
-    retrieve_json[argv[1]] = items_data
+    retrieve_json[str(id)] = items_data
 
     with open(file_name, 'w', encoding='utf-8') as file_json:
         dump(retrieve_json, file_json)
 
 
 if __name__ == '__main__':
-    export_json()
+    export_json(int(argv[1]))
